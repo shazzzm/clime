0a9b23a087c5abbb3a79bdb94b111f3953bef0be
Tristan Millington <tristan.millington@gmail.com>
2018-10-30 11:53:50 +0000

    Added cross validation

--- clime.py
+++ clime.py
@@ -1,12 +1,18 @@
 import numpy as np
 import cvxpy as cvx
+import sklearn.model_selection as skms
 
-def solve(S, l):
+def solve(X, l, verbose=False):
     """
     Solves the CLIME problem for empirical covariance matrix S
     """
+    S = np.cov(X, rowvar=False)
+    n = X.shape[0]
     p = S.shape[0]
     theta = np.zeros((p, p))
+    if p > n:
+        rho = np.sqrt(np.log10(p/n))
+        S = S + rho * np.eye(p)
     for i in range(p):
         beta = cvx.Variable(p)
         objective = cvx.Minimize(cvx.norm(beta, 1))
@@ -14,7 +20,7 @@ def solve(S, l):
         e[i] = 1
         constraints = [cvx.norm(S @ beta - e, "inf") <= l]
         prob = cvx.Problem(objective, constraints)
-        result = prob.solve()
+        result = prob.solve(solver=cvx.ECOS, verbose=verbose)
         theta[i, :] = beta.value
 
     return _make_symmetric(theta)
@@ -39,4 +45,42 @@ def _make_symmetric(M):
                 M[i, j] = y
                 M[j, i] = y
 
-    return M
+    return M  
+
+def _log_likelihood(S, theta):
+    sgn, logdet = np.linalg.slogdet(theta)
+    return np.trace(theta * S) - sgn * logdet
+
+def cross_validation(X, max_l=0.8, min_l=None, num_lambdas=25, num_splits = 6):
+    """
+    Runs cross validation with CLIME to figure out the optimal lambda value
+    """
+    p = X.shape[1]
+    n = X.shape[0]
+    if min_l is None:
+        if p > n:
+            min_l = 0.2
+        else:
+            min_l = 0.4
+
+    lambdas = np.logspace(np.log10(min_l), np.log10(max_l), num_lambdas)
+    mean_likelihood = np.zeros(num_lambdas)
+    for j,l in enumerate(lambdas):
+        likelihoods = np.zeros(num_splits)
+        for i in range(num_splits):
+            X_train, X_test = skms.train_test_split(X, test_size=0.25)
+            theta = solve(X_train, l)
+            S_test = np.cov(X_test, rowvar=False)
+            likelihoods[i] = _log_likelihood(S_test, theta)
+        print(likelihoods)
+        mean_likelihood[j] = np.mean(likelihoods)
+
+    print(mean_likelihood)
+    min_err_lambda = np.argmin(mean_likelihood)
+    return solve(X, lambdas[min_err_lambda]), lambdas[min_err_lambda]
+
+
+    
+
+    
+    
--- test_clime.py
+++ test_clime.py
@@ -3,13 +3,13 @@ from sklearn.datasets import make_sparse_spd_matrix
 import numpy as np
 import matplotlib.pyplot as plt
 
-p = 100
-l = 1
+p = 5
 prec = make_sparse_spd_matrix(p, 0.7)
 C = np.linalg.inv(prec)
-X = np.random.multivariate_normal(np.zeros(p), C, 200)
+X = np.random.multivariate_normal(np.zeros(p), C, 100)
 S = np.cov(X, rowvar=False)
-clime_precision = clime.solve(S, l)
+clime_precision, l = clime.cross_validation(X)
+clime_precision[np.abs(clime_precision) < 0.0001] = 0
 plt.figure()
 plt.title("Empirical Precision")
 plt.imshow(np.linalg.inv(S), cmap='hot', interpolation='nearest')
